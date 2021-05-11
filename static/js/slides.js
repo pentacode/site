59050cfacf76f434100e4f4fa86eb5d38eb30717
Martin Kleinschrodt <martin@maklesoft.com>
2021-05-11 11:47:12 +0200

    WIP

---
+++ static/js/slides.js
@@ -0,0 +1,73 @@
+(() => {
+    function initSection(section) {
+        const buttons = section.querySelectorAll(".tab");
+        const slidesContainer = section.querySelector(".slides");
+        const slides = [...section.querySelectorAll(".slides-slide")];
+        const images = section.querySelectorAll(".slides img");
+        const containerWidth = slidesContainer.offsetWidth;
+
+        const scroll = () => {
+            for (const [i, slide] of slides.entries()) {
+                const button = buttons[i];
+                const width = slide.offsetWidth;
+                const left = slide.offsetLeft;
+                const img = images[i];
+
+                const offset =
+                    Math.abs(slidesContainer.scrollLeft - left + (containerWidth - width) / 2) / containerWidth;
+
+                img.style.transform = `translateZ(${-offset * 300}px)`;
+
+                const active = Math.abs(offset) < 0.15;
+                // img.style["z-index"] = active ? "1" : "0";
+                slide.style["pointer-events"] = active ? "none" : "";
+                button.classList.toggle("active", active);
+            }
+        };
+
+        function selectSlide(i) {
+            const slide = slides[i];
+            console.log("select slide", i, slide);
+            slidesContainer.scrollLeft = slide.offsetLeft - (containerWidth - slide.offsetWidth) / 2;
+        }
+
+        function buttonMouseEnter(button) {
+            const inner = button.querySelector(".tab-inner");
+            anime({ targets: button, scale: 1.05 });
+            anime({ targets: button, height: inner.offsetHeight, easing: "easeInOutQuart", duration: 200 });
+        }
+
+        function buttonMouseLeave(button) {
+            anime({ targets: button, scale: 1.0 });
+            anime({ targets: button, height: 100, easing: "easeInOutQuart", duration: 200 });
+        }
+
+        slidesContainer.addEventListener("scroll", scroll);
+        scroll();
+
+        for (const [i, button] of buttons.entries()) {
+            button.addEventListener("click", () => selectSlide(i));
+            button.onmouseenter = (e) => buttonMouseEnter(e.target);
+            button.onmouseleave = (e) => buttonMouseLeave(e.target);
+        }
+
+        for (const [i, slide] of slides.entries()) {
+            slide.addEventListener("click", () => selectSlide(i));
+        }
+
+        selectSlide(0);
+    }
+
+    function init() {
+        const sections = document.querySelectorAll(".section-slides");
+        for (const section of sections) {
+            initSection(section);
+        }
+    }
+
+    if (["complete", "loaded", "interactive"].includes(document.readyState)) {
+        init();
+    } else {
+        window.addEventListener("DOMContentLoaded", () => init());
+    }
+})();
